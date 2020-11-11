cc4efd85a7fa20221fa1292700adfb8dcbf0ab86
sarahlcox <sarahl.cox.22@gmail.com>
2020-11-11 16:52:51 -0600

    added html routes

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/htmlroutes.js"));
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
