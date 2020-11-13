2c648e45e5c76510b3dcc1125a5b1f7e314082af
sarahlcox <sarahl.cox.22@gmail.com>
2020-11-12 18:55:29 -0600

    made changes to api

--- public/api.js
+++ public/api.js
@@ -13,8 +13,8 @@ const API = {
   async addExercise(data) {
     const id = location.search.split("=")[1];
 
-    const res = await fetch("/api/addExercise/" + id, {
-      method: "POST",
+    const res = await fetch("/api/workouts/" + id, {
+      method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data)
     });
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,21 @@
 const router = require("express").Router();
 const Workouts = require("../models/workout.js");
 
+router.get("/api/workouts", (req, res) => {
+  console.log("hello!");
+  // finds all documents from our db
+  Workouts.find({})
+  .then(response => {
+    res.json(response);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
 // adds exercises to previous workout plan
 router.post("/api/workouts", ({ body }, res) => {
+  console.log(body);
   Workouts.create(body)
     .then(response => {
       res.json(response);
@@ -12,26 +25,27 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
-router.post("/api/addExercise/:id", (req, res) => {
+router.put("/api/workouts/:id", (req, res) => {
 // tell the db that we want to get the workout of the id (req.params.id or req.id? rec.body.id?) and then add to its exercises what we have in req.body
-    Workouts.find({})
-    // ^^ in find we need to tell it what to find in the {id:req.body.id}
-    .then(response => {
-    // want to take this response and parse it into the workouts object (ex.... workout.exercises.push/add(req.body), and then update to the db
-    })
-    .catch(err => {
-    res.status(400).json(err);
-    });
-    console.log(req.body);
+  Workouts.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body}})
+// ^^ in find we need to tell it what to find in the {id:req.body.id}
+  .then(response => {
+  console.log(response);
+  res.json(response);
+// want to take this response and parse it into the workouts object (ex.... workout.exercises.push/add(req.body), and then update to the db
+  })
+  .catch(err => {
+  res.status(400).json(err);
   });
+// console.log(req.body);
+});
 
 
 // View the combined weight of multiple exercises on stats page
 router.get("/api/workouts/range", (req, res) => {
     Workouts.find({})
-    .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+    .then(response => {
+      res.json(response);
     })
     .catch(err => {
       res.status(400).json(err);
